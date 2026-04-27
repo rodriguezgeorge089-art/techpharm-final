572cd55edca8a56510bbdd63abfd392f46b9f1e6
rodriguezgeorge089-art <rodriguezgeorge089@gmail.com>
2026-04-27 21:47:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+html = """<!DOCTYPE html>
+<html><head><title>Login</title></head><body>
+<h2>Login</h2>
+<form action="/login" method="post">
+  <input name="email" placeholder="Email"><br>
+  <input type="password" name="password" placeholder="Password"><br>
+  <button type="submit">Log In</button>
+</form>
+<p>Don't have an account? <a href="/signup">Sign up</a></p>
+</body></html>"""
+
 @app.get("/")
 def home():
-    return HTMLResponse("<h1>It works!</h1>")
+    return HTMLResponse(html)
